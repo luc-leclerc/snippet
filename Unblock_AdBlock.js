64c435dff526dd267c6a5fba0fe5c7dd35116558
undefined <undefined@null>
2018-01-31 22:12:30 -0500

    Added technologyreview.com

--- Unblock_AdBlock.js
+++ Unblock_AdBlock.js
@@ -1,14 +1,18 @@
-(()=>{if (window.location.hostname.indexOf('www.businessinsider.com') >= 0) {
+javascript:(()=>{if (window.location.hostname.indexOf('www.businessinsider.com') >= 0) {
 	document.getElementsByClassName('tp-modal')[0].parentNode
 			.removeChild(document.getElementsByClassName('tp-modal')[0]);
 	document.getElementsByClassName('tp-backdrop tp-active')[0].parentNode.removeChild(document
 			.getElementsByClassName('tp-backdrop tp-active')[0]);
 	document.getElementsByTagName('body')[0].classList.remove('tp-modal-open');
+} else if (window.location.hostname.indexOf("journalstar.com") >= 0) {
+	document.getElementsByClassName('fc-dialog-overlay')[0].parentNode.removeChild(document
+			.getElementsByClassName('fc-dialog-overlay')[0]);
+	document.getElementsByTagName('body')[0].style["overflow-y"] = "auto";
+} else if (window.location.hostname.indexOf("technologyreview.com") >= 0){
+	var wall = document.getElementsByClassName('incognito-wall')[0];
+	wall.parentNode.removeChild(wall);
+	document.getElementsByTagName('html')[0].style["overflow"] = "auto";
 } else {
 	console.log('not effective here');
 }
-if (window.location.hostname.indexOf("journalstar.com") >= 0) {
-	document.getElementsByClassName('fc-dialog-overlay')[0].parentNode.remove(document
-			.getElementsByClassName('fc-dialog-overlay')[0]);
-	document.getElementsByTagName('body')[0].style["overflow-y"] = "auto";
-}})();
+})();
