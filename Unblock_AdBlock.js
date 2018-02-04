4c93fa397451abbad95a5598d63af594969ee670
undefined <undefined@null>
2018-02-03 20:35:47 -0500

    Remove blur classes

--- Unblock_AdBlock.js
+++ Unblock_AdBlock.js
@@ -14,7 +14,59 @@ javascript:(()=>{if (window.location.hostname.indexOf('www.businessinsider.com')
 	document.getElementsByTagName('html')[0].style["overflow"] = "auto";
 } else if (window.location.hostname.indexOf("telegraph.co.uk") >= 0) {
 	document.getElementsByClassName('adblocker-message')[0].parentNode.removeChild(document.getElementsByClassName('adblocker-message')[0]);
+} else if (window.location.hostname.indexOf("menshealth.com") >= 0) {
+	document.getElementById('block-zeus-newsletter-ad-block-signup').parentNode.removeChild(document.getElementById('block-zeus-newsletter-ad-block-signup'));
 } else {
 	console.log('not effective here');
 }
+
+removeBlur();
+
+function removeBlur() {
+	/* Find every unique class */
+	var classSet = {};
+	var all = document.getElementsByTagName("*");
+	for (var i = 0; i < all.length; i++) {
+		var curElement = all[i];
+		if (curElement && curElement.classList && curElement.classList.length) {
+			for (var j = 0; j < curElement.classList.length; j++) {
+				var curClass = curElement.classList[j];
+				classSet[curClass] = 1;
+			}
+		}
+		curElement.style["filter"] ="";
+		curElement.style["-webkit-filter"] ="";
+	}
+
+	/* Test presence of blur on each class */
+	var testDiv = document.createElement('div');
+	for (var className in classSet) {
+		if (!classSet.hasOwnProperty(className)) {
+			continue;
+		}
+		testDiv.setAttribute('class', className); 
+		
+		/* check blur presence */
+		var computedStyle = window.getComputedStyle(testDiv, null);
+		var filterVal = computedStyle.getPropertyValue('filter') + "";
+		var webkitFilterVal = computedStyle.getPropertyValue('-webkit-filter') + "";
+		if (filterVal.indexOf('blur') >= 0 || webkitFilterVal.indexOf('blur') >= 0) {
+			classSet[className] = 2;
+		}
+	}
+	
+	/* Remove offending class from all objects. */
+	for (var className in classSet) {
+		if (!classSet.hasOwnProperty(className)) {
+			continue;
+		}
+		if (classSet[className] == 2) {
+			for (var i = 0; i < all.length; i++) {
+				var curElement = all[i];
+				curElement.classList.remove(className);
+			}
+		}
+	}
+}
+
 })();
